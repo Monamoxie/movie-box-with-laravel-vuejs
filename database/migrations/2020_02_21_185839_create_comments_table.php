aa8ee5936f4181a0b34707c3671c199dd23e9ea3
Mona Moxie <moxie4lyf@gmail.com>
2020-02-21 20:13:09 +0100

    Fixed faker array randomizer

---
+++ database/migrations/2020_02_21_185839_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->uuid('movie_id');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+
+            $table->string('comment');
+
+            $table->bigInteger('poster')->unsigned();
+            $table->foreign('poster')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
