48c04edcef56701ed829839e89b56da3774c2b12
Mona Moxie <moxie4lyf@gmail.com>
2020-02-22 18:18:41 +0100

    Updated Movie service

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -83,17 +83,3 @@ class MovieService
     }
     
 }
-
-
-0216dcef025fd3e5fa6fcede2f618e1b.jpg  b041a6e3338664fda114064fc8bc6119.jpg
-06e48ad404226ac94e2a5538d8062426.jpg  banner2.jpg
-1.jpg                                 banner22.jpg
-2cb1e9b8b26c1b730e74b460ac36e5e3.jpg  circle_ci_test.png
-3c514570e62adde029bae60ab1ef9d1f.png  lady-happy-removebg-preview.png
-50b025396aca31b6c0462a6174656f17.jpg  logo.png
-7da0438f03388c05e592a83ca55e4598.jpg  logo2.png
-9aaf1cc7a08bd6830b132fc58d5260a2.jpg  look2.png
-a81f1e99f1272eb3591db73e7f54cd5d.jpg
-
-
-a81f1e99f1272eb3591db73e7f54cd5d.jpg
