d8700917ac3b71b6404529f77e7e8386960070e0
Mona Moxie <moxie4lyf@gmail.com>
2020-02-22 18:51:07 +0100

    README update

--- resources/views/movies/landing.blade.php
+++ resources/views/movies/landing.blade.php
@@ -53,10 +53,13 @@
         <div class="overlay">
             <div class="container">
                 <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p class="lead">
+                    In a land of myth, and a time of magic, the destiny of a great kingdom rests on the shoulders of a young boy. His name....
+                    Buhari.
+                </p>
                 <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <p>It uses utility methods for ensuring and reassuring that nothing I just typed makes any sense. </p>
+                <a class="btn btn-primary btn-lg" href="javascript:void(0)" role="button">Learn more</a>
             </div>
         </div>
     </div>
