e46a2161ec8c02bb4d9c8a0670334a75105b41b0
Mona Moxie <moxie4lyf@gmail.com>
2020-02-22 10:42:37 +0100

    Re arranged movie jumbotrons

--- resources/views/movies/landing.blade.php
+++ resources/views/movies/landing.blade.php
@@ -48,7 +48,22 @@
         </div>
 
     </div>
+
+    <div class="jumbotron">
+        <div class="overlay">
+            <div class="container">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        </div>
+    </div>
+    
 @endsection
+
+
 
 @section('custom_script')
     <script type="text/javascript" src="{{ asset('js/movies_list.js') }}"></script>    
