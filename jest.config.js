9d1d99d892e0e4a4aa4cb9dbdd20a04a6aea66a4
Mona Moxie <moxie4lyf@gmail.com>
2020-05-05 14:57:21 +0100

    configured jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   // preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  testRegex: 'tests/Unit/Frontend/.*.spec.js$'
+  testRegex: 'tests/Unit/Frontend/.*.spec.js$',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  'transform': {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
 }
